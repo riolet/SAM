f81b23eed939e78ea8bfc9e7efb4e87bde67ca34
Joe Pelz <joe.pelz@gmail.com>
2016-07-06 16:47:58 -0700

    updated db config

--- code.py
+++ code.py
@@ -1,7 +1,18 @@
+import sys
 import web
 # tell renderer where to look for templates
 render = web.template.render('templates/')
-db = web.database(dbn='mysql', user='root', pw='Zervos', db='samapper')
+
+
+try:
+    sys.dont_write_bytecode = True
+    import dbconfig_local as dbconfig
+    sys.dont_write_bytecode = False
+except Exception as e:
+    print e
+    import dbconfig
+
+db = web.database(dbn='mysql', user=dbconfig.params['user'], pw=dbconfig.params['passwd'], db=dbconfig.params['db'], port=dbconfig.params['port'])
 
 # Manage routing from here. Regex matches URL and chooses class by name
 urls = (
