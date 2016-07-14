cb782c731d1f64734fdf03fdf57739309c794d6e
Joe <joe.pelz@gmail.com>
2016-07-13 17:13:54 -0700

    positioned nodes better

--- preprocess.py
+++ preprocess.py
@@ -5,6 +5,7 @@ Preprocess the data in the database's upload table Syslog
 import common
 import dbaccess
 import random
+import math
 
 
 def clean_tables():
@@ -140,139 +141,193 @@ def check_collisions(node, nodelist, margin = 0):
     return False
 
 
+def group_nodes_for_layout(node_iter):
+    nodes = list(node_iter)
+    placed = []
+    parent = -1
+    temp = []
+    groups = []
+    for node in nodes:
+        if node.parent == parent:
+            temp.append(node)
+            continue
+        else:
+            # placed.extend(layout_nodes(temp))
+            groups.append(temp)
+            temp = [node]
+            parent = node.parent
+    groups.append(temp)
+
+    for group in groups:
+        placed.extend(layout_nodes(group))
+
+    return placed
+
+
+def layout_nodes(nodes):
+    placed = []
+
+    if len(nodes) == 0:
+        pass
+
+    elif len(nodes) == 1:
+        node = nodes[0];
+        node.x = node.px
+        node.y = node.py
+        placed.append(node)
+
+    elif len(nodes) < 6:  # 1..6 => circle
+        i = 0.0
+        limit = len(nodes)
+        for node in nodes:
+            node.x = math.sin(i / limit * math.pi * 2) * node.pr * 0.5 + node.px
+            node.y = math.cos(i / limit * math.pi * 2) * node.pr * 0.5 + node.py
+            i += 1
+            placed.append(node)
+
+    elif len(nodes) < 16:  # 6..15 => double circle
+        split = int(round(len(nodes) / 3.0))
+        inner = nodes[:split]
+        outer = nodes[split:]
+        i = 0.0
+        limit = len(inner)
+        for node in inner:
+            node.x = math.sin(i / limit * math.pi * 2) * node.pr * 0.4 + node.px
+            node.y = math.cos(i / limit * math.pi * 2) * node.pr * 0.4 + node.py
+            i += 1
+            placed.append(node)
+
+        i = 0.0
+        limit = len(outer)
+        for node in outer:
+            node.x = math.sin(i / limit * math.pi * 2) * node.pr * 0.9 + node.px
+            node.y = math.cos(i / limit * math.pi * 2) * node.pr * 0.9 + node.py
+            i += 1
+            placed.append(node)
+
+    elif len(nodes) < 50:  # 16..50 => random
+        for node in nodes:
+            node.x = random.random() * node.pr * 1.8 - node.pr * 0.9 + node.px
+            node.y = random.random() * node.pr * 1.8 - node.pr * 0.9 + node.py
+            placed.append(node)
+
+    elif len(nodes) < 255:  # 51..255 => grid
+        nodesWide = int(len(nodes) ** 0.5)
+        w = nodes[0].pr * 1.8
+        x = -nodes[0].pr * 0.9 + nodes[0].px
+        y = -nodes[0].pr * 0.9 + nodes[0].py
+        limit = nodes[0].pr * 0.91 + nodes[0].px
+        step = w / (nodesWide - 1)
+        for node in nodes:
+            node.x = x
+            node.y = y
+            x += step
+            if x > limit:
+                x = -nodes[0].pr * 0.9 + nodes[0].px
+                y += step
+            placed.append(node)
+    return placed
+
+
 def position_nodes():
     # position the /8 first
     # arrangement radius is 20000
     # node radius is 2000
-    rows = dbaccess.getNodes()
-    placed = []
-    if len(rows) > 0:
-        placed.append(rows[0])
-        for node in rows:
-            while check_collisions(node, placed, 1000):
-                node.x = random.random() * 40000 - 20000
-                node.y = random.random() * 40000 - 20000
-            placed.append(node)
+    query = """
+        SELECT 1 AS parent, address, connections, 0 AS px, 0 AS py, 20000 AS pr, x, y
+        FROM
+            Nodes8
+        ORDER BY connections DESC;
+        """
+    rows = common.db.query(query)
 
-        for node in placed:
-            query = """
-            UPDATE Nodes8
-            SET x = $nx
-              , y = $ny
-            WHERE address = $nip"""
-            qvars = {'nx': node.x,
-                     'ny': node.y,
-                     'nip': node.address}
-            common.db.query(query, vars=qvars)
+    placed = group_nodes_for_layout(rows)
+
+    for node in placed:
+        query = """
+        UPDATE Nodes8
+        SET x = $nx
+          , y = $ny
+        WHERE address = $nip"""
+        qvars = {'nx': node.x,
+                 'ny': node.y,
+                 'nip': node.address}
+        common.db.query(query, vars=qvars)
 
     # position the /16 within each node's parent
     query = """
-        SELECT A.parent8, A.address, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
+        SELECT A.parent8 AS parent, A.address, A.connections, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
         FROM
             Nodes16 A JOIN Nodes8 B
             ON A.parent8 = B.address
-        ORDER BY A.parent8;
+        ORDER BY parent, connections DESC;
         """
     rows = common.db.query(query)
-    placed = []
-    if len(rows) > 0:
-        parent = -1
-        for node in rows:
-            if node.parent8 != parent:
-                parent = node.parent8
-                node.x = node.px
-                node.y = node.py
-                placed.append(node)
-            else:
-                node.x = random.random() * node.pr * 2 - node.pr + node.px
-                node.y = random.random() * node.pr * 2 - node.pr + node.py
-                placed.append(node)
-        for node in placed:
-            query = """
-            UPDATE Nodes16
-            SET x = $nx
-              , y = $ny
-            WHERE parent8 = $pip8 AND address = $nip"""
-            qvars = {'nx': node.x,
-                     'ny': node.y,
-                     'pip8': node.parent8,
-                     'nip': node.address}
-            common.db.query(query, vars=qvars)
+
+    placed = group_nodes_for_layout(rows)
+
+    for node in placed:
+        query = """
+        UPDATE Nodes16
+        SET x = $nx
+          , y = $ny
+        WHERE parent8 = $pip8 AND address = $nip"""
+        qvars = {'nx': node.x,
+                 'ny': node.y,
+                 'pip8': node.parent,
+                 'nip': node.address}
+        common.db.query(query, vars=qvars)
 
     # position the /24 within each node's parent
     query = """
-        SELECT A.parent8, A.parent16, A.address, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
+        SELECT A.parent8, A.parent16 AS parent, A.address, A.connections, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
         FROM
             Nodes24 A JOIN Nodes16 B
             ON (A.parent16 = B.address AND A.parent8 = B.parent8)
-            ORDER BY A.parent8, A.parent16;
+        ORDER BY parent8, parent, connections DESC;
         """
     rows = common.db.query(query)
-    placed = []
-    if len(rows) > 0:
-        parent = -1
-        for node in rows:
-            # TODO: comparing parent16 by itself may backfire if sequential nodes have the same /16 and different /8
-            #       such as 128.192.x.x followed by 129.192.x.x
-            if node.parent16 != parent:
-                parent = node.parent16
-                node.x = node.px
-                node.y = node.py
-                placed.append(node)
-            else:
-                node.x = random.random() * node.pr * 2 - node.pr + node.px
-                node.y = random.random() * node.pr * 2 - node.pr + node.py
-                placed.append(node)
-        for node in placed:
-            query = """
-            UPDATE Nodes24
-            SET x = $nx
-              , y = $ny
-            WHERE parent8 = $pip8 AND parent16 = $pip16 AND address = $nip"""
-            qvars = {'nx': node.x,
-                     'ny': node.y,
-                     'pip8': node.parent8,
-                     'pip16': node.parent16,
-                     'nip': node.address}
-            common.db.query(query, vars=qvars)
+
+    placed = group_nodes_for_layout(rows)
+
+    for node in placed:
+        query = """
+        UPDATE Nodes24
+        SET x = $nx
+          , y = $ny
+        WHERE parent8 = $pip8 AND parent16 = $pip16 AND address = $nip"""
+        qvars = {'nx': node.x,
+                 'ny': node.y,
+                 'pip8': node.parent8,
+                 'pip16': node.parent,
+                 'nip': node.address}
+        common.db.query(query, vars=qvars)
 
     # position the /32 within each node's parent
     query = """
-        SELECT A.parent8, A.parent16, A.parent24, A.address, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
+        SELECT A.parent8, A.parent16, A.parent24 AS parent, A.address, A.connections, B.x AS px, B.y AS py, B.radius AS pr, A.x, A.y
         FROM
             Nodes32 A JOIN Nodes24 B
             ON (A.parent24 = B.address AND A.parent16 = B.parent16 AND A.parent8 = B.parent8)
-            ORDER BY A.parent8, A.parent16, A.parent24;
+        ORDER BY parent8, parent16, parent, connections DESC;
         """
     rows = common.db.query(query)
-    placed = []
-    if len(rows) > 0:
-        parent = -1
-        for node in rows:
-            # TODO: comparing parent24 by itself may backfire if sequential nodes have the same /24 and different /16
-            #       such as 192.168.16.x followed by 192.169.16.x
-            if node.parent24 != parent:
-                parent = node.parent24
-                node.x = node.px
-                node.y = node.py
-                placed.append(node)
-            else:
-                node.x = random.random() * node.pr * 2 - node.pr + node.px
-                node.y = random.random() * node.pr * 2 - node.pr + node.py
-                placed.append(node)
-        for node in placed:
-            query = """
-            UPDATE Nodes32
-            SET x = $nx
-              , y = $ny
-            WHERE parent8 = $pip8 AND parent16 = $pip16  AND parent24 = $pip24 AND address = $nip"""
-            qvars = {'nx': node.x,
-                     'ny': node.y,
-                     'pip8': node.parent8,
-                     'pip16': node.parent16,
-                     'pip24': node.parent24,
-                     'nip': node.address}
-            common.db.query(query, vars=qvars)
+
+    placed = group_nodes_for_layout(rows)
+
+    for node in placed:
+        query = """
+        UPDATE Nodes32
+        SET x = $nx
+          , y = $ny
+        WHERE parent8 = $pip8 AND parent16 = $pip16  AND parent24 = $pip24 AND address = $nip"""
+        qvars = {'nx': node.x,
+                 'ny': node.y,
+                 'pip8': node.parent8,
+                 'pip16': node.parent16,
+                 'pip24': node.parent,
+                 'nip': node.address}
+        common.db.query(query, vars=qvars)
 
 
 def import_links():
@@ -374,9 +429,8 @@ def import_links():
 def preprocess_log():
     clean_tables()
     import_nodes()
-    import_links()
     position_nodes()
-    import_links() # this is invoked twice intentionally
+    import_links()
     # do something with links...
 
 
