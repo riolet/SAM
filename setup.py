08c8e973032c49d6fcbc0e2177c31e7ffb284327
JoePelz <joe.pelz@riolet.com>
2016-07-20 11:42:42 -0700

    prints message on success now

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ def setup():
     dbaccess.create_database()
     # add any other setup tasks here
 
+    print("\n--- Setup completed successfully ---")
+
 
 if __name__ == "__main__":
-    setup();
+    setup()
