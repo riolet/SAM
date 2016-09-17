ffc2889770e6e3534934688d261be5e972eaac58
Rohana <you@example.com>
2016-09-17 01:28:47 -0700

    Parse both inbound and outbound log lines for ASA logs

--- import_asa.py
+++ import_asa.py
@@ -25,7 +25,7 @@ class ASAImporter(BaseImporter):
             2 => error in parsing the line. It was too short for some reason
         """
         # regexp to extract from ASA syslog
-        regexp = r"^.* Built inbound (?P<asa_protocol>.*) connection (?P<asa_conn_id>\d+) for (?P<asa_src_zone>.*):(?P<asa_src_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(?P<asa_src_port>\d+) \(.*/\d+\) to (?P<asa_dst_zone>.*):(?P<asa_dst_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(?P<asa_dst_port>\d+) .*"
+        regexp = r"^.* Built (in|out)bound (?P<asa_protocol>.*) connection (?P<asa_conn_id>\d+) for (?P<asa_src_zone>.*):(?P<asa_src_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(?P<asa_src_port>\d+) \(.*/\d+\) to (?P<asa_dst_zone>.*):(?P<asa_dst_ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(?P<asa_dst_port>\d+) .*"
         m = re.match(regexp, line)
 
         if m:
