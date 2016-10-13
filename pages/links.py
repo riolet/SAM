68782e64eb324894d5e22f675d73cf3d082ee32b
JoePelz <joe.pelz@gmail.com>
2016-10-12 18:39:44 -0700

    added tests for links.py

--- pages/links.py
+++ pages/links.py
@@ -28,6 +28,18 @@ class Links:
         return json.dumps(result, default=decimal_default)
 
     def GET(self):
+        """
+        The expected GET data includes:
+            'address': comma-seperated list of dotted-decimal IP addresses.
+                Each address is only as long as the subnet,
+                    so 12.34.0.0/16 would be written as 12.34
+                A request for 1.2.3.0/24, 192.168.0.0/16, and 21.0.0.0/8
+                    would be "1.2.3,192.168,21"
+            'filter': optional. If included, only report links to this destination port.
+            'tstart': optional. Used with 'tend'. The start of the time range to report links during.
+            'tend': optional. Used with 'tstart'. The end of the time range to report links during.
+        :return:
+        """
         web.header("Content-Type", "application/json")
 
         get_data = web.input()
@@ -50,4 +62,4 @@ class Links:
                                   filter=filter,
                                   timerange=(timestart, timeend))
         else:
-            return json.dumps({})
+            return json.dumps({'result': "ERROR: no 'address' specified."})
--- pages/nodes.py
+++ pages/nodes.py
@@ -40,7 +40,8 @@ class Nodes:
                     so 12.34.0.0/16 would be written as 12.34
                 A request for 1.2.3.0/24, 192.168.0.0/16, and 21.0.0.0/8
                     would be "1.2.3,192.168,21"
-        :return: A list of the child-nodes of each of the given addresses.
+        :return: A dictionary where the keys are the supplied addresses (or _ if no address)
+            and the values are a list of child nodes.
         """
         web.header("Content-Type", "application/json")
         get_data = web.input()
