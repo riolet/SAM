5ad88f79ff5ee8979ae1b6ca97ff190394ccd23d
JoePelz <joe.pelz@gmail.com>
2016-10-12 18:01:19 -0700

    added map page test

---
+++ spec/python/test_pages_map.py
@@ -0,0 +1,12 @@
+import server
+import web
+
+app = web.application(server.urls, globals())
+
+def test_exists():
+    req = app.request('/map')
+    assert req.status == "200 OK"
+    assert req.headers['Content-Type'] == "text/html; charset=utf-8"
+    lines = req.data.splitlines()
+    assert lines[0] == "<!DOCTYPE html>"
+    assert lines[-1] == "</html>"
