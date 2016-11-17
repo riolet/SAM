296d4d61ede90d22f20360d39d70ea8d072b2492
JoePelz <joe.pelz@gmail.com>
2016-11-16 18:19:43 -0800

    opened filters panel when no filters active

--- static/js/table.js
+++ static/js/table.js
@@ -48,6 +48,8 @@ function importURL() {
     });
     if (params.hasOwnProperty("filters")) {
         filters.setFilters(decodeURIComponent(params.filters));
+    } else {
+        $(".ui.accordion").accordion("open", 0);
     }
 }
 
@@ -138,57 +140,6 @@ function hostname_edit_callback(event) {
     return false;
 }
 
-/*
-;(function () {
-    "use strict";
-    var table = {};
-    table.data = [];
-    table.html = null;
-    table.private = {};
-    table.private.tbody = null;
-    table.private.thead = null;
-    table.private.tfoot = null;
-    table.private.columns = [];
-    table.setTable = function(tableHTML) {
-        table.html = tableHTML;
-        table.private.thead = document.createElement("thead");
-        table.private.tbody = document.createElement("tbody");
-        table.private.tfoot = document.createElement("tfoot");
-        table.html.innerHTML = "";
-        table.html.appendChild(table.private.thead);
-        table.html.appendChild(table.private.tbody);
-        table.html.appendChild(table.private.tfoot);
-    };
-    table.setColumns = function(colNames) {
-        table.private.columns = colNames;
-        table.private.thead.appendChild(table.buildRow("th", colNames));
-    };
-    table.addRow = function(data) {
-        var tr = table.buildRow("TD", data);
-        tbody.appendChild(tr);
-    };
-    table.clearTable = function(data) {
-        table.private.thead.innerHTML = "";
-        table.private.tbody.innerHTML = "";
-        table.private.tfoot.innerHTML = "";
-    };
-    table.buildRow = function(type, values) {   //type is "th" or "td"
-        var tr = document.createElement("tr");
-        var entry;
-        var i = 0;
-        //This will add "undefined" td elements as needed to match number of columns.
-        for (; i < table.private.columns.length; i += 1) {
-            entry = document.createElement(type);
-            entry.appendChild(document.createTextNode(values[i]));
-            tr.appendChild(entry);
-        }
-        return tr;
-    };
-    // Export table instance to global scope
-    window.table = table;
-})();
-*/
-
 function init() {
     "use strict";
 
@@ -208,10 +159,6 @@ function init() {
     filters.displayDiv = document.getElementById("filters");
     filters.applyCallback = applyFilter;
     filters.updateDisplay();
-
-    //Configure Table
-    //table.setTable(document.getElementById("resultsTable"));
-    //table.setColumns(["Address", "Hostname", "Role", "Environment", "Tags"]);
 
     //Configure page_size buttons
     var buttonGroups = document.getElementsByClassName("buttons pagesize");
