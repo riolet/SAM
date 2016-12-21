935c4ea409bb67e391124afb3eef685dc09ab4e7
JoePelz <joe.pelz@gmail.com>
2016-12-20 16:57:31 -0800

    more SSL testing

--- spec/python/testing/test_server.py
+++ spec/python/testing/test_server.py
@@ -12,18 +12,6 @@ handler_thread = None
 shutdown_processor = threading.Event()
 
 
-class TCPRequestHandler_stream(SocketServer.StreamRequestHandler):
-    def handle(self):
-        # self.rfile is a file-like object created by the handler;
-        # we can now use e.g. readline() instead of raw recv() calls
-        self.data = self.rfile.readline().strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # Likewise, self.wfile is a file-like object used to write back
-        # to the client
-        self.wfile.write("Thanks for {0}".format(self.data.upper()))
-
-
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.rfile is a file-like object created by the handler;
