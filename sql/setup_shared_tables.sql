97a8796e184342200c8b5e5682bcc0eb3f8ece66
JoePelz <Joe.Pelz@gmail.com>
2017-02-01 13:08:48 -0800

    updated ports table

--- sql/setup_shared_tables.sql
+++ sql/setup_shared_tables.sql
@@ -9,19 +9,20 @@ CREATE FUNCTION encodeIP (ip8 INT, ip16 INT, ip24 INT, ip32 INT)
 RETURNS INT UNSIGNED DETERMINISTIC
 RETURN (ip8 * 16777216 + ip16 * 65536 + ip24 * 256 + ip32);
 
-
 -- Listing of datasources to use
 CREATE TABLE IF NOT EXISTS Datasources
 (id             INT NOT NULL AUTO_INCREMENT
+,subscription   INTEGER NOT NULL
 ,name           VARCHAR(255) NOT NULL DEFAULT "New Datasource"
 ,ar_active      TINYINT(1) NOT NULL DEFAULT 0
 ,ar_interval    INT NOT NULL DEFAULT 300
-,CONSTRAINT `pk_ds` PRIMARY KEY (`id`)
+,CONSTRAINT `PK_sds` PRIMARY KEY (`id`)
 );
 
 -- User Settings
 CREATE TABLE IF NOT EXISTS Settings
-(datasource     INT NOT NULL
+(subscription   INTEGER PRIMARY KEY
+,datasource     INT NOT NULL
 ,live_dest      INT DEFAULT NULL
 ,color_node     INT UNSIGNED NOT NULL DEFAULT 0x5555CC
 ,color_bg       INT UNSIGNED NOT NULL DEFAULT 0xAAFFDD
@@ -34,43 +35,10 @@ CREATE TABLE IF NOT EXISTS Settings
 ,CONSTRAINT `FK_sld` FOREIGN KEY (`live_dest`) REFERENCES `Datasources` (`id`)
 );
 
--- Create the Nodes table
-CREATE TABLE IF NOT EXISTS Nodes
-(ipstart           INT UNSIGNED NOT NULL
-,ipend             INT UNSIGNED NOT NULL
-,subnet            INT NOT NULL
-,alias             VARCHAR(96)
-,env               VARCHAR(64)
-,x                 FLOAT(12,3) DEFAULT 0
-,y                 FLOAT(12,3) DEFAULT 0
-,radius            FLOAT(12,3) DEFAULT 2000
-,CONSTRAINT PKNodes PRIMARY KEY (ipstart, ipend)
-,INDEX nenv (env)
-);
-
--- Create the table of tags
-CREATE TABLE IF NOT EXISTS Tags
-(ipstart           INT UNSIGNED NOT NULL
-,ipend             INT UNSIGNED NOT NULL
-,tag               VARCHAR(32)
-,CONSTRAINT PKTags PRIMARY KEY (ipstart, ipend, tag)
-,CONSTRAINT FKTags FOREIGN KEY (ipstart, ipend) REFERENCES Nodes (ipstart, ipend)
-);
-
 -- Look-Up-Table for ports and port aliases
 CREATE TABLE IF NOT EXISTS Ports
-(port              INT UNSIGNED NOT NULL
-,active            BOOL NOT NULL DEFAULT TRUE
-,tcp               BOOL NOT NULL DEFAULT TRUE
-,udp               BOOL NOT NULL DEFAULT TRUE
+(port              INT UNSIGNED NOT NULL PRIMARY KEY
+,protocols         TEXT
 ,name              VARCHAR(10) NOT NULL DEFAULT ""
 ,description       VARCHAR(255) NOT NULL DEFAULT ""
-,CONSTRAINT PKPorts PRIMARY KEY (port)
-);
-
-CREATE TABLE IF NOT EXISTS PortAliases
-(port              INT UNSIGNED NOT NULL
-,name              VARCHAR(10) NOT NULL DEFAULT ""
-,description       VARCHAR(255) NOT NULL DEFAULT ""
-,CONSTRAINT PKPortAliases PRIMARY KEY (port)
 );
