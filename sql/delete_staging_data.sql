26280941f6b3f3979def6688eb22f2755c8d3bc5
JoePelz <joe.pelz@gmail.com>
2017-02-02 11:35:24 -0800

    preprocess uses subscriptions now

--- sql/delete_staging_data.sql
+++ sql/delete_staging_data.sql
@@ -1,7 +1,4 @@
 -- this sql file is responsible for deleting all data from staging tables
 
--- delete all data from Links table
-DELETE FROM {prefix}staging_Links;
-
--- delete all data from Syslog table
-DELETE FROM {prefix}Syslog;
+DELETE FROM s{acct}_ds{id}_StagingLinks;
+DELETE FROM s{acct}_ds{id}_Syslog;
