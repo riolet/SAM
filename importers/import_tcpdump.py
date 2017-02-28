cc788134406967e5ebfc66bc59896fecb89942f6
Rohana Rezel <rohana.rezel@riolet.com>
2017-02-27 20:59:55 -0800

    Error handling for the live collector

--- importers/import_tcpdump.py
+++ importers/import_tcpdump.py
@@ -20,29 +20,31 @@ class TCPDumpImporter(BaseImporter):
         Returns:
             0 on success and non-zero on error.
         """
+        try:
+            a = line.split()
 
-        a = line.split()
+            dt = dateutil.parser.parse(a[0])
+            src_ip_port = a[2].split(".")
+            src_port = src_ip_port.pop()
+            dst_ip_port = a[4].split(".")
+            dst_port = dst_ip_port.pop()
 
-        dt = dateutil.parser.parse(a[0])
-        src_ip_port = a[2].split(".")
-        src_port = src_ip_port.pop()
-        dst_ip_port = a[4].split(".")
-        dst_port = dst_ip_port.pop()
+            dictionary['src'] = self.ip_to_int(*src_ip_port)
+            dictionary['srcport'] = int(src_port)
+            dictionary['dst'] = self.ip_to_int(*dst_ip_port)
+            dictionary['dstport'] = int(dst_port.strip(":"))
+            dictionary['timestamp'] = dt.strftime(self.mysql_time_format)
 
-        dictionary['src'] = self.ip_to_int(*src_ip_port)
-        dictionary['srcport'] = int(src_port)
-        dictionary['dst'] = self.ip_to_int(*dst_ip_port)
-        dictionary['dstport'] = int(dst_port.strip(":"))
-        dictionary['timestamp'] = dt.strftime(self.mysql_time_format)
-
-        # TODO: the following is placeholder.
-        #       Needed: test data or spec to read
-        dictionary['protocol'] = 'TCP'.upper()
-        dictionary['duration'] = '1'
-        dictionary['bytes_received'] = '1'
-        dictionary['bytes_sent'] = '1'
-        dictionary['packets_received'] = '1'
-        dictionary['packets_sent'] = '1'
+            # TODO: the following is placeholder.
+            #       Needed: test data or spec to read
+            dictionary['protocol'] = 'TCP'.upper()
+            dictionary['duration'] = '1'
+            dictionary['bytes_received'] = '1'
+            dictionary['bytes_sent'] = '1'
+            dictionary['packets_received'] = '1'
+            dictionary['packets_sent'] = '1'
+        except TypeError as type_error:
+            print "Ignoring line because of error : " + type_error.message
 
         return 0
 
