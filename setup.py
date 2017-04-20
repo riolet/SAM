1f8fe549af63600311e2ac2fe523b9c451d6010c
JoePelz <joe.pelz@gmail.com>
2017-04-19 17:25:58 -0700

    updated website link and version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='samapper',
-    version='0.2.1',
+    version='0.2.2',
     classifiers=[
             'Development Status :: 3 - Alpha',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
