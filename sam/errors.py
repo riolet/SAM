b4a71c96ee250a9a670911cf15395652e539a087
JoePelz <joe.pelz@gmail.com>
2017-05-23 12:20:49 -0700

    fixed traceback for error messages

--- sam/errors.py
+++ sam/errors.py
@@ -4,13 +4,15 @@ class SubscriptionMissingError(Exception): pass
 class LoggedOutError(Exception): pass
 
 
-class MalformedRequest(Exception): pass
+class MalformedRequest(Exception):
+    def __init__(self, message):
+        Exception.__init__(self, message)
 
 
 class RequiredKey(MalformedRequest):
     def __init__(self, name, key, *args, **kwargs):
-        MalformedRequest.__init__(self, args, kwargs)
         self.message = "Missing key: {name} ('{key}') not specified.".format(name=name, key=key)
+        MalformedRequest.__init__(self, self.message)
 
 
 class AuthenticationError(Exception): pass
