6f373b9f427cc1d90733868bba0e9e839a227ed4
JoePelz <joe.pelz@gmail.com>
2017-08-02 12:06:10 -0700

    added french placeholder files

---
+++ sam/local/fr.py
@@ -0,0 +1,88 @@
+map_title = "Map"
+meta_title = "Host Details"
+stats_title = "Stats"
+table_title = "Table View"
+settings_title = "Settings"
+dashboard_title = "Dashboard"
+
+units_role_cc = "client"
+units_role_c = "mostly client"
+units_role_cs = "mixed client/server"
+units_role_s = "mostly server"
+units_role_ss = "server"
+units_kilo = "K"
+units_mega = "M"
+units_giga = "G"
+units_bytes = "B"  # bytes
+units_kbytes = "KB"
+units_mbytes = "MB"
+units_gbytes = "GB"
+units_tbytes = "TB"
+units_bps = "B/s"  # bytes per second
+units_kbps = "KB/s"
+units_mbps = "MB/s"
+units_gbps = "GB/s"
+units_tbps = "TB/s"
+units_pps = "p/s"  # packets per second
+units_kpps = "Kp/s"
+units_mpps = "Mp/s"
+units_gpps = "Gp/s"
+units_tpps = "Tp/s"
+stats_udips = "Unique destination IP addresses:"
+stats_usips = "Unique source IP addresses:"
+stats_uips = "Unique IP addresses:"
+stats_ports = "Unique destination ports used:"
+stats_sports = "Unique system ports used (0..1023):"
+stats_uports = "Unique user ports used (1024..49151):"
+stats_pports = "Unique private ports used (49152..65535):"
+stats_ports_max = "Max ports for one destination:"
+stats_ports_few = "Percent of destinations with fewer than 10 ports: "
+stats_conns = "Total Number of distinct connections (node -> node:port) stored:"
+stats_conns_many = "Number of distinct connections occurring more than 100 times:"
+stats_hosts = "Total hosts recorded"
+stats_overall = "Overall"
+stats_datasource = "Datasource: {}"
+table_col_address = "Address"
+table_col_alias = "Hostname"
+table_col_conn_in = "Total inbound connections"
+table_col_conn_out = "Total outbound connections"
+table_col_role = "Role (0 = client, 1 = server)"
+table_col_environment = "Environment"
+table_col_tags = "Tags"
+table_col_bytes = "Bytes handled"
+table_col_packets = "Packets handled"
+table_col_protocols = "Protocols used"
+table_proto_i = "(in)"  # in (inbound)
+table_proto_o = "(out)"  # out (outbound)
+table_proto_io = "(i/o)"  # in / out
+table_spread = "Results: {0} to {1}"
+table_spread_none = "No matching results."
+meta_none = "No host found at this address"
+meta_src = "Source IP"
+meta_dst = "Dest. IP"  # Destination IP
+meta_port = "Dest. Port"  # Destination Port
+meta_ports = "Port Accessed"
+meta_links = "Count / Min"
+meta_protocols = "Protocols"
+meta_sum_bytes = "Sum Bytes"
+meta_avg_bytes = "Avg Bytes"
+meta_sum_packets = "Sum Packets"
+meta_avg_packets = "Avg Packets"
+meta_avg_duration = "Avg Duration"
+meta_child_ip = "Address"
+meta_child_name = "Name"
+meta_child_count = "Active Endpoints"
+meta_child_ratio = "Role (0=client, 1=server)"
+
+
+
+
+
+
+
+
+
+
+
+
+
